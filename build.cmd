1eb186aea143bbfb375c874a0b4439f316c9d23b
Richard Moss <richard.moss@cyotek.com>
2022-07-09 12:43:48 +0100

    FIX: Updated build.cmd to handle changed distribution

--- build.cmd
+++ build.cmd
@@ -12,6 +12,8 @@ CALL %CTKBLDROOT%SetupEnv.cmd
 REM Build and sign the file
 %msbuildexe% Cyotek.QuickScan.sln /p:Configuration=Release /verbosity:minimal /nologo /t:Clean,Build
 
+IF EXIST dist DEL dist\*.* /q
+
 IF NOT EXIST dist MKDIR dist
 
 PUSHD .\dist
@@ -19,12 +21,13 @@ PUSHD .\dist
 copy /y ..\src\bin\release\ctkqscan.exe
 copy /y ..\src\bin\release\ctkqscan.exe.config
 copy /y ..\src\bin\release\ctkqscan.pdb
+copy /y ..\src\bin\release\ctkqscan.default.ini
 copy /y ..\src\bin\release\Cyotek.Windows.Forms.ImageBox.dll
-copy /y ..\src\bin\release\INIFileParser.dll
+copy /y ..\src\bin\release\Cyotek.Data.Ini.dll
 copy /y ..\src\bin\release\LICENSE.txt
 copy /y ..\src\bin\release\README.md
 
-CALL signcmd ctkqscan.exe
+CALL sign-program ctkqscan.exe
 
 %zipexe% a Cyotek.QuickScan.1.0.x.zip -r
 
